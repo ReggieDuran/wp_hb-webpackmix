8be2a5f66e7dca295fec41181890e65b6dce10b7
Reggie <reggieduran15@gmail.com>
2021-09-05 22:34:27 +0800

    Fixed slider banner

--- components/banner-slider.php
+++ components/banner-slider.php
@@ -4,8 +4,9 @@
         if( have_rows('slider') ):
             while( have_rows('slider') ) : the_row();
                 ?>
-                <div class="slider-item">
-                    <div class="slider-image" <?= get_sub_field('image') ? 'style="background: url(' . get_sub_field('image')['url'] . ');"' : '' ?>></div>
+                <div class="slider-item" <?= get_sub_field('background_image') ? 'style="background: url(' . get_sub_field('background_image')['url'] . ');"' : '' ?>>
+                    <div class="overlay-bg"></div>
+                    
                     <div class="slider-content">
                         <?= get_sub_field('slider_content'); ?>
                     </div>
